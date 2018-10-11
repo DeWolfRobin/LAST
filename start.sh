7a17d90e7dbb70f35ebdce80cd3ef6d37c19ffdf
root <root@localhost.localdomain>
2018-10-11 14:05:04 +0200

    fixed sub

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 # /bin/sh
 clear
 echo "Starting Tool\n"
-nmap -sn -iL nmap-ips.txt -oX pingscanresult.txt
-python xml2jsonSub/xml2json.py -t xml2json -o pingresult.json pingscanresult.txt
+nmap -n -sS -iL nmap-ips.txt -oG - | awk '/Up$/{print $2}' > scope.txt
+nmap -sV -O -iL scope.txt -oX nmapresult.txt
+python xml2json/xml2json.py -t xml2json -o nmapresults.json nmapresult.txt
 #while true
 #do
 #echo -n "."
