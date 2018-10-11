1fb27ba1db098ed22b67cd0299da06f1784a154b
root <root@localhost.localdomain>
2018-10-11 13:54:32 +0200

    submodule

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 clear
 echo "Starting Tool\n"
 nmap -sn -iL nmap-ips.txt -oX pingscanresult.txt
-python xml2json/xml2json.py -t xml2json -o pingresult.json pingscanresult.txt
+python xml2jsonSub/xml2json.py -t xml2json -o pingresult.json pingscanresult.txt
 #while true
 #do
 #echo -n "."
