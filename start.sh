58dd384881a70d4efea227aae874af1621cb3c61
root <root@localhost.localdomain>
2018-10-11 14:19:38 +0200

    nmap works

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 clear
 echo "Starting Tool\n"
 nmap -n -sS -iL nmap-ips.txt -oG - | awk '/Up$/{print $2}' > scope.txt
+echo "done with pingsweep"
 nmap -sV -O -iL scope.txt -oX nmapresult.txt
+echo "done with service detection"
 python xml2json/xml2json.py -t xml2json -o nmapresults.json nmapresult.txt
 #while true
 #do
