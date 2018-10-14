48234fd352e5bc054986826e674a668808e66e3f
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-10-14 12:57:43 +0200

    added colors

--- nessus.sh
+++ nessus.sh
@@ -1,1 +1,5 @@
-msfconsole -q -x "load nessus;nessus_connect admin:admin@localhost:8834"
+/etc/init.d/nessusd start
+load="load nessus;"
+connect="nessus_connect admin:admin@localhost:8834"
+final=$load+$connect;
+msfconsole -n -q -x $final
--- start.sh
+++ start.sh
@@ -1,17 +1,26 @@
 # /bin/sh
 
 #CONFIG
-$conf = "config/nmap.conf"
-$hosts = "output/live-hosts.txt"
-$xml = "output/nmap-output.xml"
-$xml2json = "plugins/xml2json/xml2json.py"
-$json = "output/nmap-output.json"
+conf="config/nmap.conf"
+hosts="output/live-hosts.txt"
+xml="output/nmap-output.xml"
+xml2json="plugins/xml2json/xml2json.py"
+json="output/nmap-output.json"
+red="\e[31m"
+reset="\e[0m"
+bold="\e[1m"
+flicker="\e[5m"
+lgreen="\e[92m"
 
 #SCRIPT
 clear
-echo "Starting Tool\n"
-nmap -n -sS -iL config/nmap.conf -oG - | awk '/Up$/{print $2}' > output/live-hosts.txt
-echo "Pingsweep done"
-nmap -sV -O -iL output/live-hosts.txt -oX output/nmap-output.xml
-echo "Service detection done"
-python plugins/xml2json/xml2json.py -t xml2json -o output/nmap-output.json output/nmap-output.xml
+echo $red$bold"Starting Tool\n"$reset
+nmap -n -sS -iL $conf -oG - | awk '/Up$/{print $2}' > $hosts
+echo $bold$lgreen"Pingsweep done"$reset
+#nmap -sV -O -iL $hosts -oX $xml
+echo $bold$lgreen"Service detection done"$reset
+#python $xml2json -t xml2json -o $json $xml
+echo $bold$lgreen"Starting nessus scan"$reset
+#sh nessus.sh
+echo $lgreen$bold$flicker"Nessus scan in progress"$reset
+# check periodically to see if scan is done
