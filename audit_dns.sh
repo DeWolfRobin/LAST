22381e150bccfe4ae0a07d560d6efe5dedf39dd0
root <root@localhost.localdomain>
2018-10-25 05:51:49 -0400

    Added basic dnsrecon script

---
+++ audit_dns.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# $domain should come from masterscript (maybe in scope
+dnscan(){
+  dnsrecon -d $1 -D /usr/share/wordlists/dnsmap.txt -t axfr -j $(pwd)/dnsinfo.json
+}
+
+dnscan 'localhost'
