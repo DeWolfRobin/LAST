a27bcc4655d728523db616e5dcd28d56f666ea0b
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-10-26 19:33:03 +0200

    added nessus check if nessus is ready

--- nessus.sh
+++ nessus.sh
@@ -1,3 +1,12 @@
+# check if nessus is ready
+ready=$(http --verify=no https://localhost:8834/scans \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".scans")
+while [ "$ready" = null ]
+do
+	echo "waiting for nessus"
+	sleep 1
+	ready=$(http --verify=no https://localhost:8834/scans \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".scans")
+done
+
 # get hosts seperated by comma's
 hosts=$(sed ':a;N;$!ba;s/\n/,/g' output/live-hosts.txt)
 
