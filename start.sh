6795ec6128c3bd963329b25a780398eb0d340def
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-11-05 08:34:14 +0100

    Included scrips in master script and added examples folder

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@ nmapvulnjson="output/nmapvuln.json"
 nmapvuln2json="output/nmapvuln2.json"
 xml2json="plugins/xml2json/xml2json.py"
 json="output/nmap-output.json"
+domain="localhost"
+
+## COLORS
 red="\e[31m"
 reset="\e[0m"
 bold="\e[1m"
@@ -31,8 +34,10 @@ echo $bold$lgreen"Pingsweep done"$reset
 nmap -sV -O -iL $hosts -oX $xml
 echo $bold$lgreen"Service detection done"$reset
 python $xml2json -t xml2json -o $json $xml
+echo $bold$lgreen"Starting DNS scan"$reset
+dnsscan $domain
 echo $bold$lgreen"Starting nessus scan"$reset
-sh nessus.sh
+nessusscan
 echo $lgreen$bold$flicker"Nessus scan in progress"$reset
 # check periodically to see if scan is done
 echo $bold$lgreen"Starting nmap vulnerability scan"$reset
@@ -41,3 +46,53 @@ nmap -sV --script vulscan.nse -iL $hosts -oX $nmapvuln2xml
 python $xml2json -t xml2json -o $nmapvulnjson $nmapvulnxml
 python $xml2json -t xml2json -o $nmapvuln2json $nmapvuln2xml
 
+## FUNCTIONS
+
+### DNS Scan
+# FEATURE REQUEST: $domain should be autodetected and filled with all domains OR should be loaded from config
+dnsscan(){
+  dnsrecon -d $1 -D /usr/share/wordlists/dnsmap.txt -t axfr -j $(pwd)/dnsinfo.json
+}
+
+### Nessus Scan
+nessusscan(){
+# check if nessus is ready
+ready=$(http --verify=no https://localhost:8834/scans \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".folders")
+while [ "$ready" = null ]
+do
+	echo "waiting for nessus"
+	sleep 1
+	ready=$(http --verify=no https://localhost:8834/scans \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".folders")
+done
+
+# get hosts seperated by comma's
+hosts=$(sed ':a;N;$!ba;s/\n/,/g' output/live-hosts.txt)
+
+# create a new nessus scan using the predefined policy uuid
+scanid=$(echo "{\"uuid\": \"ad629e16-03b6-8c1d-cef6-ef8c9dd3c658d24bd260ef5f9e66\",\"settings\": {\"name\": \"AutoScan\",\"description\": \"a scan created by the automated script\",\"scanner_id\": 1,\"enabled\": \"true\",\"starttime\": \"20170820T100500\",\"launch\": \"YEARLY\",\"text_targets\": \"${hosts}\"}}" | http --verify=no POST https://localhost:8834/scans \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".scan.id")
+echo $scanid
+
+# start the scan
+http -v --verify=no POST https://localhost:8834/scans/$scanid/launch \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624"
+
+# check periodically if the scan is finished and get the export id
+scandone=$(echo "{\"format\": \"html\", \"chapters\": \"vuln_hosts_summary;vuln_by_host;compliance_exec;remediations;vuln_by_plugin;compliance\"}" | http --verify=no POST https://localhost:8834/scans/$scanid/export \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".file")
+while [ "$scandone" = null ]
+do
+	echo "waiting for scan"
+	sleep 1
+	scandone=$(echo "{\"format\": \"html\", \"chapters\": \"vuln_hosts_summary;vuln_by_host;compliance_exec;remediations;vuln_by_plugin;compliance\"}" | http --verify=no POST https://localhost:8834/scans/$scanid/export \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".file")
+done
+echo $scandone
+
+# export is done, then save the export
+status=$(http --verify=no https://localhost:8834/scans/$scanid/export/$scandone/status \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".status")
+echo $status
+while [ "$status" = loading ]
+do
+	echo "waiting for export"
+	sleep 1
+	status=$(http --verify=no https://localhost:8834/scans/$scanid/export/$scandone/status \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" | jq -r ".status")
+done
+http --verify=no https://localhost:8834/scans/$scanid/export/$scandone/download \X-ApiKeys:"accessKey=cc052c8b010d0aa933ab8af73043d12d643e2db2953962a39e37b4af9875a150; secretKey=6ec8e1a103944e64a1f296afa005d9646637f9091d21016a9f07c66deb7f4624" > output/nessus-output.html
+}
