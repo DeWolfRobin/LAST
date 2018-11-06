a570136e04c4f73fdf827797ef604f682c40f72b
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-11-06 14:46:54 +0100

    added rpc scan

--- start.sh
+++ start.sh
@@ -98,7 +98,20 @@ http --verify=no https://localhost:8834/scans/$scanid/export/$scandone/download 
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 
-### Recon-ng
+### 
+rpc(){
+out=$(rpcclient -U '' -N $1 -c querydominfo | sed 's/\t//g' | sed '$!s/$/","/g' | sed 's/:/":"/g')
+array=$(rpcclient -U '' -N $1 -c enumdomusers | sed 's/rid:\[.*$//g' | sed '$!s/$/,/g' | sed 's/user://g' | sed 's/ //g;s/\[//g;s/\]//g')
+out="{\"${out}\",\"users\":\"[${array}]\"}"
+echo $out > output/rpc.json
+}
+
+additionalscan(){
+# for each live host complete a set of scans
+#for here
+#rpc <IP>
+#end for
+}
 
 
 #SCRIPT
