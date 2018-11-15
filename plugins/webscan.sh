2fbe208dedacc8022a8d93caefcc60d6927764f8
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-11-15 11:06:44 +0100

    more webscan

--- plugins/webscan.sh
+++ plugins/webscan.sh
@@ -8,6 +8,9 @@ echo "Working internet connection detected"
 fi
 }
 
+dirsearcher(){
+	python3 plugins/dirsearch/dirsearch.py -e php,asp,aspx,jsp,html,zip,jar,sql -u $1
+}
 
 niktoscan() {
 	nikto -host $1 -port $2 -Format xml -output output/nikto.xml
@@ -15,8 +18,10 @@ niktoscan() {
 }
 
 checklive() {
-if (curl -s $1:$2) then
-	echo "GOOD"
+if (curl -s $1:$2 --connect-timeout 5) then
+	echo $1" is active on port "$2
+	niktoscan $1 $2
+	dirsearcher $1:$2
 else
 	echo "host unreachable"
 fi
@@ -28,7 +33,7 @@ main(){
 	while read ip; do
 		for port in "${ports[@]}"
 		do
-			echo $ip":"$port
+			checklive $ip $port
 		done
 	done <output/live-hosts.txt
 }
