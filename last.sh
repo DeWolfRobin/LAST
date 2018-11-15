4eaad88f46bdc729da626c259b78b75f44ca98ec
DeWolfRobin <robin.de.wolf@student.howest.be>
2018-11-15 09:11:17 +0100

    better now

--- last.sh
+++ last.sh
@@ -144,6 +144,29 @@ echo "Working internet connection detected"
 fi
 }
 
+getscope(){
+nmcli dev show wlan0 | grep IP4.ADDRESS | cut -d ':' -f2 | tr -d '[:space:]' > config/nmap.conf
+}
+
+webscan(){
+ports=80,443,8080,8443
+ips=$(cat output/live-hosts.txt | sed 's/\n/,/g')
+rm output/nikto.xml
+
+niktoscan() {
+	nikto -host $1 -port $2 -Format xml -output output/nikto.xml
+	python plugins/xml2json/xml2json.py -t xml2json -o output/nikto.json output/nikto.xml
+}
+
+checklive() {
+if (curl -s $1:$2) then
+	echo "GOOD"
+else
+	echo "host unreachable"
+fi
+}
+}
+
 #SCRIPT
 clear
 ## SETUP
